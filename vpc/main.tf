b5621fa3eeb5e70771f3cdac42a8a971ec159b4c
Valentechno <valentino.agbetoglo.study@gmail.com>
2023-07-29 23:09:20 -0400

    vpc main.tf file corrected

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "vpc" {
   cidr_block                  = var.cidr_block
-  enable_enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support          = var.enable_dns_support
 }
 
