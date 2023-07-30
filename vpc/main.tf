7f9f3d263f915831319bc25d2d0be670dfd97dc0
Valentechno <valentino.agbetoglo.study@gmail.com>
2023-07-29 23:19:47 -0400

    vpc main.tf correction

--- vpc/main.tf
+++ vpc/main.tf
@@ -40,7 +40,7 @@ resource "aws_subnet" "private-subnet1" {
 resource "aws_subnet" "private-subnet2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidr2
-  count             = var.create_resource_private_subnet_2
+  count             = var.create_resource_private_subnet_2 ? 1 : 0
   availability_zone = var.availability_zone_us-east-2b
 
   tags = {
@@ -77,21 +77,21 @@ resource "aws_route_table" "public_route_table_2" {
 resource "aws_route_table_association" "public_1_rta" {
   count = var.create_resource_public_subnet_1 ? 1 : 0
 
-  subnet_id      = aws_subnet.public-subnet1.id
-  route_table_id = aws_route_table.public_route_table_1.id
+  subnet_id      = aws_subnet.public-subnet1[count.index].id
+  route_table_id = aws_route_table.public_route_table_1[count.index].id
 }
 
 resource "aws_route_table_association" "public_2_rta" {
   count = var.create_resource_private_subnet_2 ? 1 : 0
 
-  subnet_id      = aws_subnet.public-subnet2.id
-  route_table_id = aws_route_table.public_route_table_2.id
+  subnet_id      = aws_subnet.public-subnet2[count.index].id
+  route_table_id = aws_route_table.public_route_table_2[count.index].id
 }
 
 resource "aws_route" "public_igw_1" {
   count = var.create_resource_public_subnet_1 ? 1 : 0
 
-  route_table_id         = aws_route_table.public_route_table_1.id
+  route_table_id         = aws_route_table.public_route_table_1[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.IGW.id
 }
@@ -99,7 +99,7 @@ resource "aws_route" "public_igw_1" {
 resource "aws_route" "public_igw_2" {
   count = var.create_resource_public_subnet_2 ? 1 : 0
 
-  route_table_id         = aws_route_table.public_route_table_2.id
+  route_table_id         = aws_route_table.public_route_table_2[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.IGW.id
 }
